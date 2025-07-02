0654bc2f13859ad32d6b59d06cdb189c5d7ecec1
Ayushmanbinaryflux <127283361+Ayushmanbinaryflux@users.noreply.github.com>
2025-07-02 18:06:25 +0530

    Update script.py

--- packages/parsers/syslog_rfc_3164/script.py
+++ packages/parsers/syslog_rfc_3164/script.py
@@ -1,7 +1,7 @@
 import re as re3164
 
 def parse(data):
-    match = re3164.match(r"<(\d+)>(\w{3} \d{1,2} \d{2}:\d{2}:\d{2}) ([\w.-]+) (.*)", data)
+    match = re3164.match(r"<(\d+)>(\w{3}\s+\d{1,2} \d{2}:\d{2}:\d{2}) ([\w.-]+) (.*)", data)
     if not match:
         raise ValueError("Invalid RFC 3164 format")
     return {
